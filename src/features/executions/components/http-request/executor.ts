6e9040f3976942eda2b97bd54de5dbf490d78a57
nishant-shah <112845053+nishant-spendflo@users.noreply.github.com>
2025-11-09 18:41:48 +0545

    19: Node variables

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,6 +3,7 @@ import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
 
 type HttpRequestData = {
+  variableName?: string;
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body?: string;
@@ -19,6 +20,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     // TODO: Publish "error" state for HTTP request
     throw new NonRetriableError("HTTP Request node: No endpoint configured.");
   }
+  if (!data.variableName) {
+    // TODO: Publish "error" state for HTTP request
+    throw new NonRetriableError("Variable name not configured.");
+  }
 
   const result = await step.run("http-request", async () => {
     const endpoint = data.endpoint!;
@@ -27,6 +32,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     const options: KyOptions = { method };
     if (["POST", "PUT", "PATCH"].includes(method)) {
       options.body = data.body;
+      options.headers = {
+        "Content-Type": "application/json",
+      };
     }
     const response = await ky(endpoint, options);
     const contentType = response.headers.get("content-type");
@@ -34,13 +42,24 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       ? await response.json()
       : await response.text();
 
-    return {
-      ...context,
+    const responsePayload = {
       httpResponse: {
         status: response.status,
         statusText: response.statusText,
         data: responseData,
       },
+    };
+    if (data.variableName) {
+      return {
+        ...context,
+        [data.variableName]: responsePayload,
+      };
+    }
+
+    // Fallback to direct httpResponse for backward compatibility
+    return {
+      ...context,
+      ...responsePayload,
     };
   });
 
